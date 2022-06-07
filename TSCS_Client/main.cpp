442b12eade96fdd9de740a9562517a59298e8df0
Ahmed Elkhateeb <ahmed.elkhateeb@poulttech.com>
2022-06-07 18:44:01 +0200

    added necessary files in the client and network library to test their interaction

--- TSCS_Client/main.cpp
+++ TSCS_Client/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include "Base.hpp"
 int main(void)
 {
-    std::cout << "Hello, I am client";
+    std::cout << "Hello, I am client\n";
+    std::cout <<static_cast<int>(NetworkingEnum::one)<<std::endl;
 }
---
+++ TSCS_Network/include/Base.hpp
@@ -0,0 +1,7 @@
+#pragma once
+enum class NetworkingEnum
+{
+    one, 
+    two,
+    three
+};
