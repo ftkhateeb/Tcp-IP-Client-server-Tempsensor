8f1f45875eb08f461b5dcb32766bf1de0ff89961
Ahmed Elkhateeb <ahmed.elkhateeb@poulttech.com>
2022-06-07 20:09:57 +0200

    Integrated boost library into the project

--- TSCS_Network/include/Base.hpp
+++ TSCS_Network/include/Base.hpp
@@ -1,7 +1,12 @@
 #pragma once
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
 enum class NetworkingEnum
 {
     one, 
     two,
     three
 };
+void print(const boost::system::error_code& /*e*/,
+    boost::asio::steady_timer* t, int* count);
--- TSCS_Network/src/Base.cpp
+++ TSCS_Network/src/Base.cpp
@@ -1,2 +1,16 @@
 #include <Base.hpp>
 
+void print(const boost::system::error_code& /*e*/,
+    boost::asio::steady_timer* t, int* count)
+{
+  if (*count < 5)
+  {
+    std::cout << *count << std::endl;
+    ++(*count);
+
+    t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+    t->async_wait(boost::bind(print,
+          boost::asio::placeholders::error, t, count));
+  }
+}
+
