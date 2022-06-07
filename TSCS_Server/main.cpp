11a5ac5501a341339f75faf2f677a87cdcb5bac8
Ahmed Elkhateeb <ahmed.elkhateeb@poulttech.com>
2022-06-07 18:56:45 +0200

    Updated necessary files inside the server application to test it's functionality with the networking library

--- TSCS_Server/main.cpp
+++ TSCS_Server/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include "Base.hpp"
 int main(void)
 {
-    std::cout << "Hello, I am Server";
+    std::cout << "Hello, I am Server\n";
+    std::cout <<static_cast<int>(NetworkingEnum::two)<<std::endl;
 }
